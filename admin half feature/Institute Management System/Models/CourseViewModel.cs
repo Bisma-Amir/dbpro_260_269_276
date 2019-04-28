99fb55bea70fa0b129fa921238ff28e4882bb258
Bisma-Amir <38394856+Bisma-Amir@users.noreply.github.com>
2019-04-28 23:24:20 +0500

    admin

---
+++ admin half feature/Institute Management System/Models/CourseViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Institute_Management_System
+{
+    public class CourseViewModel
+    {
+        private string title;
+        private string instructor;
+        private DateTime start_date;
+        private string course_duration;
+        private int fee;
+        private string department;
+        public string Title { get => title; set => title = value; }
+        public string Instructor { get => instructor; set => instructor = value; }
+        public DateTime Start_date { get => start_date; set => start_date = value; }
+        public string Course_duration { get => course_duration; set => course_duration = value; }
+        public int Fee { get => fee; set => fee = value; }
+        public string Department { get => department; set => department = value; }
+        
+    }
+}
---
+++ admin half feature/Institute Management System/Models/SearchViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Institute_Management_System.Models
+{
+    public class SearchViewModel
+    {
+
+        private string type;
+        private string type1;
+
+        public string Type { get => type; set => type = value; }
+        public string Type1 { get => type1; set => type1 = value; }
+    }
+}

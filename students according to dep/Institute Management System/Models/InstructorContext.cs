492aa9cad73389393cc94565887e5a946e7f43ce
Menahal Hassan <menahalhassan27@gmail.com>
2019-04-28 21:05:49 +0500

    Admin Features

---
+++ students according to dep/Institute Management System/Models/InstructorContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+
+namespace Institute_Management_System.Models
+{
+    public class InstructorContext :DbContext
+    {
+        public void  DBContext()
+        {
+            Database.SetInitializer<DbContext>(new DropCreateDatabaseIfModelChanges<DbContext>());
+        }
+        public DbSet<InstructorViewModel> TbInstructor { get; set; }
+    }
+}
---
+++ students according to dep/Institute Management System/Models/StudentViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Institute_Management_System.Models
+{
+    public class StudentViewModel
+    {
+        private string name;
+        private string cNIC;
+        private string email;
+        private string address;
+        private string department;
+        private string password;
+
+
+        public string Name { get => name; set => name = value; }
+        public string CNIC { get => cNIC; set => cNIC = value; }
+        public string Email { get => email; set => email = value; }
+        public string Address { get => address; set => address = value; }
+        public string Department { get => department; set => department = value; }
+        public string Password { get => password; set => password = value; }
+
+    }
+}
